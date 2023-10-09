be4a4fd168f06285d714f7397c460f92419c8b37
rtocastro <ricktorres@live.com>
2023-10-09 06:48:50 -0700

    added comment to javascript file

--- script.js
+++ script.js
@@ -1,48 +1,60 @@
 // Assignment code here
+
+// added variables to establish a parameter for password criteria 
+
 var specialcharacters = "!@#$*";
 var numbers = '0123456789';
 var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var lowercase = 'abcdefghijklmnopqrstuvwxyz';
 
-let userSelection = []; 
+// addded variables for user input/password gerneration
+
+let userSelection = [];
 let finalPassword = [];
 
-function generatePassword () {
+// created a function  to prompt and keep prompting rather than my original plan
+//-of creating input for each prompt that way to create a less "buggy" code
+
+function generatePassword() {
   var passwordLength = prompt("Please Enter A Number between 8-128 characters");
 
-  if(passwordLength < 8 || passwordLength > 128){
+  if (passwordLength < 8 || passwordLength > 128) {
     alert('PLEASE ENTER A NUMBER BETWEEN 8-128');
-    return 
+    return
   }
 
   var specialCharChosen = confirm('CONFIRM SPECIAL CHARACTERS TO USED "!@#$*"');
-  if(specialCharChosen){
+  if (specialCharChosen) {
     userSelection.push(...specialcharacters)
   }
 
   var numbersChosen = confirm('CONFIRM NUMBERS TO USED "0123456789"');
-  if(numbersChosen){
+  if (numbersChosen) {
     userSelection.push(...numbers)
   }
 
   var lowercasechosen = confirm('CONFRIM LOWER CASE LETTERS');
-  if(lowercasechosen){
+  if (lowercasechosen) {
     userSelection.push(...lowercase)
   }
 
   var uppercasechosen = confirm('CONFRIM UPPER CASE LETTERS');
-  if(uppercasechosen){
+  if (uppercasechosen) {
     userSelection.push(...upperCase)
   }
 
   console.log('user selection -> ', userSelection);
 
-  for (let i = 0; i <passwordLength; i++) {
+  for (let i = 0; i < passwordLength; i++) {
     finalPassword.push(userSelection[Math.floor(Math.random() * passwordLength)])
 
     console.log('final password', finalPassword)
-    
+
   }
+
+  // the solution kept coming out with commas so I had to take them away.
+  // this is where I found the reference, but I also found in one activities here
+  // "https://javascript.plainenglish.io/how-to-turn-an-array-into-a-string-without-commas-in-javascript-241598bb054b"
   
   var myNewString = finalPassword.join("");
   console.log(myNewString);
